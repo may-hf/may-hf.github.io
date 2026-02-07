7033bbb26ef52c4246f7cefb9f061ade1c6f2044
may-hf <may5meiro22maze@gmail.com>
2026-02-07 11:13:34 +0900

    lightbox.js の更新

--- lightbox.js
+++ lightbox.js
@@ -46,3 +46,20 @@
     if (e.key === "Escape" && box.classList.contains("open")) close();
   });
 })();
+
+const toTopBtn = document.getElementById("toTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    toTopBtn.style.display = "block";
+  } else {
+    toTopBtn.style.display = "none";
+  }
+});
+
+toTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
